28f00cfafa704e699bd7c8b33777a81001812c37
aucuba <aoki.11235813@ezweb.ne.jp>
2018-08-23 23:50:28 +0900

    add case

--- times.php
+++ times.php
@@ -1,12 +1,20 @@
 <?php
-$greet = function( $name ){
-    printf("Hello-> %s\r\n", $name);
+$greet = function(){
+    printf("Hello\r\n");
 };
+$test = 'test';
 $times = 5;
-range_try( $times, $greet );
-function range_try( $times, $func ) {
-    $range = range( 1, $times );
-    foreach ( $range as $number ) {
-        $func( $number );
+
+$range_try = new range_try();
+$range_try->range_try_function( $times, $greet );
+$range_try->range_try_function( $times, function () { 
+    echo 'Hello'."\n"; 
+});
+class range_try {
+    function range_try_function( $times, $func ) {
+        $range = range( 1, $times );
+        foreach ( $range as $number ) {
+            $func();
+        }
     }
 }
