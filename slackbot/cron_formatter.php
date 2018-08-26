f11e6db26ffe3e7422b2130dddd006ce52d46b29
aucuba <aoki.11235813@ezweb.ne.jp>
2018-08-27 00:44:16 +0900

    add function

---
+++ slackbot/cron_formatter.php
@@ -0,0 +1,13 @@
+<?php
+
+$cron_formatter = new cron_formatter();
+$cron_formatter->cron_format("20:00");
+class cron_formatter
+{
+    function cron_format( $time, $name, $date ){
+        $parsed_date =  date_parse_from_format ( "h:m",$time );
+        $hour = $parsed_date["hour"];
+        $minute = $parsed_date[ "minute" ];
+        var_dump($hour,$minute);
+    }
+}
