cf6cc7d7e3d5256b2b294f58aaa9e1941a4838da
aucuba <aoki.11235813@ezweb.ne.jp>
2018-08-29 21:01:55 +0900

    add string

--- slackbot/slack_bot_active.php
+++ slackbot/slack_bot_active.php
@@ -21,7 +21,8 @@ $week = date("w" );
 
 $github_contributions = new github_contributions();
 if( $date  == $target_hour ){
-    $message = $github_contributions->get_github_contributions( $record['name'] );
+    $contributions = $github_contributions->get_github_contributions( $record['name'] );
+    $message = $contributions + "->"+$parsed_date["name"];
     $bot = new SlackBot();
     print_r( $bot->post_message( new SlackBotInfo( $url, $message ) ) );
 }
