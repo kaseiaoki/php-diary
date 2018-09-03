248269e6b30c006e12717d88afd2491537dd4861
aucuba <aoki.11235813@ezweb.ne.jp>
2018-09-04 00:12:21 +0900

    9/4

---
+++ lyrics_snatch/http_get.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * http_Get
+ *
+ * @access public
+ * @param string url
+ * @return 
+ * @see holiday.php ..etc
+ */
+function http_get($url){
+    $option = [
+        CURLOPT_RETURNTRANSFER => true, 
+    ];
+
+    $curl = curl_init( $url );
+    curl_setopt_array( $curl, $option );
+
+    $data = curl_exec($curl);
+    $info = curl_getinfo($curl);
+
+    if ( $info['http_code'] !== 200 ) {
+        return false;
+    } else {
+        return $data;
+    }
+}
